96e45c9c79ef555236082c1ec72b7dd0d4e2fe3e
melynguy <nguyenmelynda@gmail.com>
2016-10-25 11:45:12 -0700

    adding rendering methods

--- firebase/js/app.js
+++ firebase/js/app.js
@@ -1,6 +1,5 @@
 //put the interpreter into strict mode
 "use strict";
-
 //create a new Firebase application using the Firebase
 //console, https://console.firebase.google.com/
 
@@ -12,3 +11,61 @@
 //  to the URL provided by Firebase 
 
 //paste the Firebase initialization code here
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCvjpnAtmX8c7uPWaHeojYrpd2m292t3VU",
+    authDomain: "tasks-demo-b294b.firebaseapp.com",
+    databaseURL: "https://tasks-demo-b294b.firebaseio.com",
+    storageBucket: "tasks-demo-b294b.appspot.com",
+    messagingSenderId: "605783813889"
+};
+firebase.initializeApp(config);
+
+var currentUser;
+var authProvider = new firebase.auth.GithubAuthProvider();
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        currentUser = user;
+        console.log(currentUser);
+    } else {
+        firebase.auth().signInWithRedirect(authProvider);
+    }
+});
+
+var taskForm = document.querySelector(".new-task-form");
+var taskTitleInput = taskForm.querySelector(".new-task-title");
+var taskList = document.querySelector(".task-list");
+
+var tasksRef = firebase.database().ref("tasks");
+
+taskForm.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+
+    var task = {
+        title: taskTitleInput.value.trim(),
+        done: false,
+        createOn: firebase.database.ServerValue.TIMESTAMP,
+        createdBy: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            email: currentUser.email
+        }
+    };
+    tasksRef.push(task);
+
+    taskTitleInput.value = "";
+
+    return false;
+});
+
+function renderTask(snapshot) {
+    
+}
+
+function render(snapshot) {
+    taskList.innerHTML = "";
+    snapshot.forEach(renderTask);
+}
+
+taskRef.on("value", render);
