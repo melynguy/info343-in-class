cc28463827871e9cb1a609c1bf89656a9d8a909e
melynguy <nguyenmelynda@gmail.com>
2016-11-17 10:34:58 -0800

    redux practice

--- redux/src/app.jsx
+++ redux/src/app.jsx
@@ -4,23 +4,33 @@ import React from "react";
 import "./css/main.css";
 
 import {Link, IndexLink} from "react-router";
-import {store} from "/.shared-state.js";
+
+//import just the Redux store from our ./shared-state.js module
+import {store} from "./shared-state.js";
 
 export default class extends React.Component {
     constructor(props) {
         super(props);
+
+        //initialize our state to be the current state
+        //of the Redux store
         this.state = store.getState();
     }
 
     componentDidMount() {
+        //just like over in favorite-list.jsx, subscribe to the store
+        //and update our state whenever the store's state changes
         this.unsub = store.subscribe(() => this.setState(store.getState()));
     }
 
     componentWillUnmount() {
-        this.unsub;
+        //unsubscribe from the store
+        this.unsub();
     }
 
     render() {
+        //use the store's state to show how many items are in the
+        //favorites array
         return (
             <div>
                 <nav>
@@ -32,8 +42,7 @@ export default class extends React.Component {
                         </li>
                         <li>
                             <Link to="/favorites" activeClassName="active">
-                                Favorites (
-                                {this.state.favorites.length})
+                                Favorites ({this.state.favorites.length})
                             </Link>
                         </li>
                     </ul>
